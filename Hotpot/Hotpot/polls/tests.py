e9313a64f29ef5711d138c888295bce41a97b3e5
Zhimao Liu <zhimao_liu@icloud.com>
2016-10-08 06:00:17 -0400

    Add files via upload
    Basic Version of Hotpot (starts from a poll toy system)

---
+++ Hotpot/Hotpot/polls/tests.py
@@ -0,0 +1,20 @@
+
+import datetime
+
+from django.utils import timezone
+from django.test import TestCase
+
+from polls.models import Question
+
+
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
---
+++ Hotpot/Hotpot/run.sh
@@ -0,0 +1,2 @@
+echo "\n\n\n\nEnter 127.0.0.1:8000/polls in your browser!!!!\n\n\n\n"
+python manage.py runserver
