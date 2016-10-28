4bb8f51d6b7e8ebbe8f4c222ba96a23962dffc49
Zhimao Liu <zhimao_liu@icloud.com>
2016-10-27 22:00:17 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
     url(r'^login/', views.login, name='login'),
     url(r'^signup/', views.signup, name='signup'),
+    url(r'^about/', views.about, name='about'),
     url(r'^authlogin/', views.auth_and_login, name='authlogin'),
     url(r'^authsignup/', views.auth_and_signup, name='authsignup'),
     url(r'^profile/', views.profile, name='profile'),
