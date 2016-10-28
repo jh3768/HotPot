812b31d665425055ed57606ca611d17bf3a9d860
rondogency <dd208@sina.com>
2016-10-27 20:12:24 -0400

    add product table

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
-                ('username', models.CharField(max_length=100)),
+                ('username', models.CharField(max_length=50)),
                 ('price', models.IntegerField(default=0)),
                 ('description', models.CharField(max_length=200)),
             ],
