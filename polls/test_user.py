37f9dc5dcb9db1529a10564c1ff0449bcbebc890
jh3768 <jh3768@columbia.edu>
2016-11-05 16:19:45 -0400

    new test_user.py

--- polls/test_user.py
+++ polls/test_user.py
@@ -1,14 +1,24 @@
 #tests.py
 #import models
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase, Client
 from django.contrib.auth.models import User
 
 class TestUser(TestCase):
     def setUp(self):
-        user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+        self.user = User.objects.create_user('temporary@gmail.com', 'temporary@gmail.com', 'temporary')
+        print (User.objects.values_list('username', flat=True))
+        
+        #self.client = Client()
 
     def test_secure_page(self):
-        self.client.login(username='temporary', password='temporary')
-        response = self.client.get('/manufacturers/', follow=True)
-        user = User.objects.get(username='temporary')
-        self.assertEqual(response.context['email'], 'temporary@gmail.com')
+        self.client.login(username='temporary@gmail.com', password='temporary')
+        response = self.client.get('/profile/', follow=True)
+        newuser = User.objects.get(username='temporary@gmail.com')
+        print (response.context)
+        print (newuser)
+        print (self.user)
+        self.assertEqual(newuser, 'temporary@gmail.com')
+        #self.assertEqual(response.context['E'], 'temporary@gmail.com')
+
+
+
