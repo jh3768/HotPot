6bc6fd7c9cb01f4daafde4b77cd49c145ad65964
jh3768 <jh3768@columbia.edu>
2016-11-05 17:23:58 -0400

    Update test_user.py

--- polls/test_user.py
+++ polls/test_user.py
@@ -7,18 +7,20 @@ class TestUser(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('temporary@gmail.com', 'temporary@gmail.com', 'temporary')
         self.c = Client()
-        print (User.objects.values_list('username', flat=True))
-        
+        self.login = self.c.login(username='temporary@gmail.com', password='temporary')
         #self.client = Client()
 
-    def test_secure_page(self):
-        login = self.c.login(username='temporary@gmail.com', password='temporary')
-        response = self.c.get('/polls/profile/', follow=True)
+    def test_login(self):
         self.user = User.objects.get(username='temporary@gmail.com')
-        self.assertTrue(login)
+        self.assertTrue(self.login)
         self.assertEqual(self.user.username, 'temporary@gmail.com')
         self.assertEqual(self.user.email, 'temporary@gmail.com')
+
+
+    def test_user_auth(self):
+        self.assertIn('_auth_user_id', self.c.session)
+
+
+    def test_login_status(self):
+        response = self.c.get('/polls/profile/', follow=True)
         self.assertEqual(response.status_code, 200)
-
-
-
