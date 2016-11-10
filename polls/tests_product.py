46c6f7b95737db44756ae7dcaa82c93930d312b2
zyntop2014 <zyntop@gmail.com>
2016-11-10 12:24:42 -0800

    add delete product in dabase test

--- polls/tests_product.py
+++ polls/tests_product.py
@@ -2,13 +2,14 @@ from django.test import TestCase
 
 from polls.models import Product
 
+
 class ProductTestCase(TestCase):
     def setUp(self):
         Product.objects.create(name="which class is the most difficult", price=100, description= "yanan")
 	
         
     def test_product_name(self):
-        """Animals that can speak are correctly identified"""
+     
         q = Product.objects.get(name= "which class is the most difficult", price =100)
 	
         self.assertEqual(q.name,  "which class is the most difficult")
@@ -18,14 +19,14 @@ class ProductTestCase(TestCase):
 
 
     def create_question(self, name="only a test", price =100):
-        return Product.objects.create(name= "which class is the most difficult", price=100)
+        return Product.objects.create(name= name, price=price)
 
     def test_whatever_creation(self):
         w = self.create_question()
         self.assertTrue(isinstance(w, Product))
 
-
-
-
-
-
+    def test_delete_question (self):
+        w = self.create_question()
+        Product.objects.filter(name = w.name).delete()
+        
+        self.assertFalse(Product.objects.filter(name = "only a test").exists())
