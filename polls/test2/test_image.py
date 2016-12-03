f7a0e718d8e42b02209ba912e6e109a9aaf892f1
yanan <zyntop@gmail.com>
2016-12-03 12:23:23 -0800

    update test imgae

--- polls/test2/test_image.py
+++ polls/test2/test_image.py
@@ -4,25 +4,23 @@ from polls.models import Product, Image
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.files import File
 
+
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         img = Image()
 
-        img.name = "test1"
+        img.name = "test3"
 
-        img.pic = SimpleUploadedFile(name='test1', content=open("polls/static/media/php.jpeg", 'rb').read(), content_type='image/jpeg')
-
+        img.pic = SimpleUploadedFile(name='test3', content=open("polls/test2/test2.png", 'rb').read(), content_type='image/png')
+        #p = Image.objects.get(name='test2').pic.path
+        #self.failUnless(open(p), 'file not found')
 
         #img.pic = File(open("polls/static/media/php.jpeg"))
         img.addImage()
 
-        
-        
-        #self.assertEqual(Image.objects.count(), 1)
-        p = Image.objects.get(name='test1').pic.path
-        print(p)
-        #self.assertEqual(Image.objects.get(name='test1').pic.path, "polls/static/media/php.jpeg")
-        
-        self.failUnless(open(p), 'file not found')
-        self.assertTrue(Image.objects.get(name='test1').pic.path)
+        self.assertTrue(Image.objects.filter(name='test3').exists())
+
+    def tearDown(self):
+        Image.objects.get(name='test3').delete()
+
 
