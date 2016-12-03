395493ab48887a845ef74a8d0758ea0d10777022
yanan <zyntop@gmail.com>
2016-12-03 13:37:40 -0800

    update test case

--- polls/test2/tests_product_update_fail.py
+++ polls/test2/tests_product_update_fail.py
@@ -5,7 +5,7 @@ from polls.models import Product
 
 
 class SimpleTest(TestCase):
-    def test_update_product_fail(self):
+    def test_update_product_fail1(self):
         p = Product()
         p.name = "test_product_fail"
         p.price = 100
@@ -13,9 +13,34 @@ class SimpleTest(TestCase):
         p.category= "books"    
         p.addProduct()
         self.assertTrue(Product.objects.filter(name="test_product_fail").exists())
-        p.updateProduct(my_price =-200, my_description= "user_fail2")
-        self.assertTrue(p.price, 100)
-        self.assertTrue(p.description, "user_fail2")
+        p.updateProduct(my_price =-200, my_description= "user_fail1")
+        self.assertEqual(p.price, 100)
+        self.assertEqual(p.description, "user_fail")
+
+    def test_update_product_fail12(self):
+        p = Product()
+        p.name = "test_product_fail"
+        p.price = 100
+        p.description = "user_fail"
+        p.category= "books"    
+        p.addProduct()
+        self.assertTrue(Product.objects.filter(name="test_product_fail").exists())
+        p.updateProduct(my_price =-0.0000001, my_description= "user_fail2")
+        self.assertEqual(p.price, 100)
+        self.assertEqual(p.description, "user_fail")
+
+
+    def test_update_product_fail13(self):
+        p = Product()
+        p.name = "test_product_fail"
+        p.price = 100
+        p.description = "user_fail"
+        p.category= "books"    
+        p.addProduct()
+        self.assertTrue(Product.objects.filter(name="test_product_fail").exists())
+        p.updateProduct(my_price = 10000000, my_description= "user_fail3")
+        self.assertEqual(p.price, 100)
+        self.assertEqual(p.description, "user_fail")
 
     def tearDown(self):
         Product.objects.get(name="test_product_fail").delete()
