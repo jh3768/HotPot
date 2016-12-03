411215a01a415eeff0950d52292557b9eb93ebf1
Zhimao Liu <zhimao_liu@icloud.com>
2016-12-03 16:23:58 -0500

    Test fab commit with msg

--- fabfile.py
+++ fabfile.py
@@ -2,20 +2,28 @@
 from __future__ import with_statement
 from fabric.api import local, settings, abort
 from fabric.contrib.console import confirm
+from sys import stderr
 
 def test():
     ''' run test suite '''
     with settings(warn_only=True):
         result = local('python3 manage.py test polls/test2', capture=True)
     if result.succeeded:
+        print (result.stderr)
         print("All tests passed !")
     if result.failed and not confirm("Tests failed. Continue anyway?"):
         abort("Aborting at user request.")
 
 def commit():
     ''' run test before commit '''
+    message = raw_input("Enter a git commit message:  ")
     test()
-    local("git commit -am 'Test CI' ")
+    local("git commit -am \"%s\"" % message)
+
+
+def pull():
+    local("git pull")
+
 
 def push():
     ''' run test before push '''
