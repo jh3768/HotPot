0f60151c8b260fbee9eead7a60d87422cfd2d697
yanan <zyntop@gmail.com>
2016-12-09 19:14:13 -0800

    update

--- polls/test2/test_product.py
+++ polls/test2/test_product.py
@@ -16,6 +16,17 @@ class SimpleTest(TestCase):
         p.addProduct()
         self.assertTrue(Product.objects.filter(name="test_product").exists())
 
+    def test_str_product(self):
+        p = Product()
+        p.name = "test_product"
+        p.price = 100
+        p.description = "test1"
+        p.category= "books"
+        p.username = "user1"  
+        p.url = "static/media/index.png"  
+        p.addProduct()
+        self.assertEqual(p.__str__(), p.name)
+
 
 
 
