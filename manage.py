4a95007ac74bd5982fdebd0117092288a9a1ebf9
yanan <zyntop@gmail.com>
2016-12-10 11:56:12 -0800

    test using fab

--- manage.py
+++ manage.py
@@ -4,9 +4,6 @@ import sys
 import coverage
 
 if __name__ == "__main__":
-    cov = coverage.Coverage()
-    #cov.erase()
-    cov.start()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Hotpot.settings")
     try:
         from django.core.management import execute_from_command_line
@@ -23,7 +20,18 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
+
+
+    is_testing = 'test' in sys.argv
+    if is_testing:
+        import coverage
+        cov = coverage.coverage(source = ['polls'])
+        #cov.erase()
+        cov.start()
+        
     execute_from_command_line(sys.argv)
-    cov.stop()
-    cov.save()
+    if is_testing:
+        cov.stop()
+        cov.save()
+        cov.report()
 
